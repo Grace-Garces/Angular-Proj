0a70d458dd5488558a3c7f1622ddb4259747de4d
Grace <166858250+Grace-Garces@users.noreply.github.com>
2024-08-02 13:56:06 -0300

    Add files via upload

---
+++ src/app/module/module.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from '../app.component';
+import { MenuComponent } from '../Perfil/Menu/menu/menu.component';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class ModuleModule { }
